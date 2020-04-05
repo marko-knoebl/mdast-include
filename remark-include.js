a07bf6efefc2b74b23108e9e7576a0567b3d7465
Marko Knoebl <marko.kn@gmail.com>
2020-04-06 00:45:07 +0200

    enable processing of vfiles

--- remark-include.js
+++ remark-include.js
@@ -1,9 +1,13 @@
+const path = require("path");
+
 const { mdastInclude } = require("./mdast-include");
 
 const attacher = (options = {}) => async (tree, file) => {
-  const newTree = await mdastInclude(tree, options, {
-    directory: options.directory || file.dirname || file.cwd
-  });
+  let directory = options.directory || file.dirname || file.cwd;
+  if (!directory && file.history && file.history.length > 0) {
+    directory = path.dirname(file.history[file.history.length - 1]);
+  }
+  const newTree = await mdastInclude(tree, options, { directory: directory });
   Object.assign(tree, newTree);
 };
 
--- remark-include.test.js
+++ remark-include.test.js
@@ -1,4 +1,5 @@
 const remark = require("remark");
+const vfile = require("vfile");
 
 const { remarkInclude } = require("./remark-include");
 
@@ -13,6 +14,16 @@ it("processes a file with imports relative to the current directory", async done
 it("processes a file with imports relative to a given directory", async () => {
   const processor = remark().use(remarkInclude, { directory: "test_assets" });
   const input = "@include glob_import.md\n";
+  const output = (await processor.process(input)).toString();
+  expect(output).toMatch(/par 1\n\npar 2/);
+});
+
+it("processes a vfile with relative imports", async () => {
+  const processor = remark().use(remarkInclude);
+  const input = vfile({
+    contents: "@include glob_import.md\n",
+    path: "test_assets/glob_import.md"
+  });
   const output = (await processor.process(input)).toString();
   expect(output).toMatch(/par 1\n\npar 2/);
 });
